ec43f5f52e3a8efcc70711fa837d25a1cc66f5a1
Lucas Rosa <lucas.rosa@corebiz.ag>
2024-09-08 14:31:38 -0300

    ajuste para teclado do celular não tapar os btns

--- src/components/FormRegistrationCollege/index.tsx
+++ src/components/FormRegistrationCollege/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 import * as S from "./styles";
 import { FormRegistrationCollegeProps } from "types/FormRegistrationCollegeProps";
 
@@ -21,58 +22,67 @@ export default function FormRegistrationCollege() {
   }, []);
 
   return (
-    <S.container>
-      <S.containerInputs>
-        <S.textInput
-          value={dataForm.name}
-          onChangeText={(newText) => {
-            setDataForm((item) => ({ ...item, name: newText }));
-          }}
-          keyboardType="default"
-          placeholder="Nome"
-          placeholderTextColor="white"
-          returnKeyType="done"
-        />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"} // Define o comportamento para iOS e Android
+      keyboardVerticalOffset={80} // Ajusta a altura conforme necessário
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        {/* Scrollview para tornar o conteúdo rolável */}
+        <S.container>
+          <S.containerInputs>
+            <S.textInput
+              value={dataForm.name}
+              onChangeText={(newText) => {
+                setDataForm((item) => ({ ...item, name: newText }));
+              }}
+              keyboardType="default"
+              placeholder="Nome"
+              placeholderTextColor="white"
+              returnKeyType="done"
+            />
 
-        <S.textInput
-          value={dataForm.email}
-          onChangeText={(newText) => {
-            setDataForm((item) => ({ ...item, email: newText }));
-          }}
-          keyboardType="email-address"
-          placeholder="E-mail"
-          placeholderTextColor="white"
-          returnKeyType="done"
-        />
+            <S.textInput
+              value={dataForm.email}
+              onChangeText={(newText) => {
+                setDataForm((item) => ({ ...item, email: newText }));
+              }}
+              keyboardType="email-address"
+              placeholder="E-mail"
+              placeholderTextColor="white"
+              returnKeyType="done"
+            />
 
-        <S.textInput
-          value={dataForm.cel}
-          onChangeText={(newText) => {
-            setDataForm((item) => ({ ...item, cel: newText }));
-          }}
-          keyboardType="name-phone-pad"
-          placeholder="Celular DD + Número. Ex:61984675545"
-          placeholderTextColor="white"
-          returnKeyType="done"
-        />
+            <S.textInput
+              value={dataForm.cel}
+              onChangeText={(newText) => {
+                setDataForm((item) => ({ ...item, cel: newText }));
+              }}
+              keyboardType="name-phone-pad"
+              placeholder="Celular DD + Número. Ex:61984675545"
+              placeholderTextColor="white"
+              returnKeyType="done"
+            />
 
-        <S.textInput
-          value={dataForm.cpf}
-          onChangeText={(newText) => {
-            setDataForm((item) => ({ ...item, cpf: newText }));
-          }}
-          keyboardType="numeric"
-          placeholder="CPF somente números. Ex:0123456789"
-          placeholderTextColor="white"
-          returnKeyType="done"
-        />
-      </S.containerInputs>
+            <S.textInput
+              value={dataForm.cpf}
+              onChangeText={(newText) => {
+                setDataForm((item) => ({ ...item, cpf: newText }));
+              }}
+              keyboardType="numeric"
+              placeholder="CPF somente números. Ex:0123456789"
+              placeholderTextColor="white"
+              returnKeyType="done"
+            />
+          </S.containerInputs>
 
-      <S.containerBtn>
-        <S.btnRegister onPress={() => {}}>
-          <S.btnText>Confirmar</S.btnText>
-        </S.btnRegister>
-      </S.containerBtn>
-    </S.container>
+          <S.containerBtn>
+            <S.btnRegister onPress={() => {}}>
+              <S.btnText>Confirmar</S.btnText>
+            </S.btnRegister>
+          </S.containerBtn>
+        </S.container>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
