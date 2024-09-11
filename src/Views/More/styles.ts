23822ad75dccf8dcc4992b7fe33ca2d446e17925
Lucas Rosa <lucas.rosa@corebiz.ag>
2024-09-11 16:20:21 -0300

    padding

--- src/Views/More/styles.ts
+++ src/Views/More/styles.ts
@@ -1,14 +1,14 @@
-import { RFPercentage } from 'react-native-responsive-fontsize'
+import { RFPercentage } from "react-native-responsive-fontsize";
 
-import styled from 'styled-components/native'
-import { color } from 'styles/pallete'
+import styled from "styled-components/native";
+import { color } from "styles/pallete";
 
 export const container = styled.View`
   flex: 1;
   padding: ${RFPercentage(2)}px ${RFPercentage(1.5)}px ${RFPercentage(1.5)}px
     ${RFPercentage(1.5)}px;
   background-color: ${color.interface.backgroundColor2};
-`
+`;
 
 export const itemMore = styled.TouchableOpacity`
   background-color: ${color.interface.backgroundColor};
@@ -17,10 +17,10 @@ export const itemMore = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
   justify-content: space-between;
-  padding: ${RFPercentage(2)}px;
+  padding: ${RFPercentage(2.5)}px;
   border-radius: 5px;
   margin-bottom: ${RFPercentage(1.5)}px;
-`
+`;
 
 export const title = styled.Text`
   color: ${color.interface.white};
@@ -29,4 +29,4 @@ export const title = styled.Text`
   flex: 1;
   align-items: center;
   justify-content: center;
-`
+`;
